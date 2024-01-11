584dc0c920c5c14330092b2c41f9bce80d874515
Mike-mk3 <mike666mtz@gmail.com>
2024-01-10 23:42:21 -0600

    agregacion de los verbos
    get, put,delete

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require ("express");
-const app = express ();
+const express = require("express");
+const app = express();
+app.use(express.json());
 const port = 3000;
+
+
+
 
 
 let platillos = [
@@ -13,34 +17,51 @@ let platillos = [
 
 
 
-app.get ('/', (req, res )=> {
-    res.send ('hello express');
+
+
+app.get('/', (req, res) => {
+    res.send('hello express');
 });
 
 
-
-app.get ('/platillos', (req, res )=> {
-    res.json ({
+app.get('/platillos', (req, res) => {
+    res.json({
         mensaje: "platillos disponibles",
         data: platillos
     });
 });
 
 
-app.post ('/platillos', (req, res )=> {
-    res.send ('agregar platillos');
+app.post('/platillos', (req, res) => {
+    // console.log(req.body); =hace que se vea en consolA
+    let nuevoPlatillo = req.body;
+    platillos.push(nuevoPlatillo);
+    res.json({
+        mensaje: "se agrego el platillo:",
+        data: nuevoPlatillo
+    });
 });
 
-app.put ('/platillos', (req, res )=> {
-    res.send ('editar platillos');
-});
 
-app.delete ('/platillos', (req, res )=> {
-    res.send ('borra platillos');
+app.put('/platillos', (req, res) => {
+    res.send('editar platillos');
 });
 
 
+app.delete('/platillos/:id', (req, res) => {
+    let id = parseInt (req.params.id);
+    const indice = platillos .findIndex (platillo => platillo.id === id);
+    platillos.splice(indice,1);
+    res.json ({
+        mensaje: "se elimino el platillo:",
+        data: null
+    });
+});
 
-app.listen (port, () => {
-    console.log ('escuchando http://localhost:' + port);
+
+
+
+
+app.listen(port, () => {
+    console.log('escuchando http://localhost:' + port);
 });
