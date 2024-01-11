1250ced1afa99745b1355edbb9f18e34b56c74d2
Mike-mk3 <mike666mtz@gmail.com>
2024-01-11 00:31:50 -0600

    cors
    agregue elcors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require ('cors');
 app.use(express.json());
 const port = 3000;
+app.use (cors());
 
 
 
@@ -43,18 +45,23 @@ app.post('/platillos', (req, res) => {
 });
 
 
-app.put('/platillos', (req, res) => {
+app.put('/platillos/:id', (req, res) => {
+    let id = parseInt (req.params.id);
+    let platilloResultado = platillos.find (platillo => platillo.id === id);
+    platilloResultado.nombre = req.body.nombre;
+    platilloResultado.precio = req.body.precio;
+    platilloResultado.descripcion = req.body.descripcion;
     res.send('editar platillos');
 });
 
 
 app.delete('/platillos/:id', (req, res) => {
     let id = parseInt (req.params.id);
-    const indice = platillos .findIndex (platillo => platillo.id === id);
+    const indice = platillos.findIndex (platillo => platillo.id === id);
     platillos.splice(indice,1);
     res.json ({
         mensaje: "se elimino el platillo:",
-        data: null
+        data: id
     });
 });
 
